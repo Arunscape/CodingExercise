992a74d1ee4d1272f7eb3239f03fbcc762364c9d
Arunscape <arun@woosaree.xyz>
2020-05-16 23:16:41 -0600

    code formatting

--- turtle-frontend/src/App.jsx
+++ turtle-frontend/src/App.jsx
@@ -1,44 +1,56 @@
-import React , {useState, useEffect} from 'react';
-import Chart from './Chart';
-const App =  () => {
-
+import React, { useState, useEffect } from "react";
+import Chart from "./Chart";
+const App = () => {
   const [data, setData] = useState([[]]);
 
-
-  
-  
   const handleFile = (file) => {
     const fr = new FileReader();
-    fr.onloadend = () => fetch('/turtle', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: fr.result.trim(),
-    }).then(res => res.json()).then(e => {
-      // console.log(e)
-      setData([...data, ...e.path])
-      console.log("setting data")
-      // console.log(data)
-    })
-    fr.readAsText(file)
-
-  }
+    fr.onloadend = () =>
+      fetch("/turtle", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: fr.result.trim(),
+      })
+        .then((res) => res.json())
+        .then((e) => {
+          // console.log(e)
+          setData([...data, ...e.path]);
+          console.log("setting data");
+          // console.log(data)
+        });
+    fr.readAsText(file);
+  };
 
   useEffect(() => {
-    console.log("data changed", data)
-  }, [data])
-  
-  return (
-    <>
-  <input 
-  type='file'
-  accept='.txt'
-  onChange= {(e) => {handleFile(e.target.files[0])}}
-  />
-  <Chart data={data}/>
-  </>
-)
-  }
+    console.log("data changed", data);
+  }, [data]);
 
-  export default App;
+  return (
+    <div style={{ height: "100vh" }}>
+      <div style={{ height: "5%" }}>
+        <input
+          type="file"
+          accept=".txt"
+          onChange={(e) => {
+            handleFile(e.target.files[0]);
+          }}
+        />
+        <input
+          type="range"
+          min="5"
+          max="5000"
+          value="500"
+          class="slider"
+          id="myRange"
+        ></input>
+      </div>
+      <div style={{ height: "95%" }}>
+        <Chart data={data} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- turtle-frontend/src/Chart.jsx
+++ turtle-frontend/src/Chart.jsx
@@ -1,92 +1,98 @@
-import React, { useRef, useEffect, useState } from 'react';
-import {Scatter} from 'react-chartjs-2';
-
+import React, { useRef, useEffect, useState } from "react";
+import { Scatter } from "react-chartjs-2";
 
 export default (props) => {
-    const [data, setData] = useState(props.data)
+  const [data, setData] = useState(props.data);
 
-    const  [chartData, setChartData] = useState([])
-    const [index, setIndex] = useState(1)
+  const [chartData, setChartData] = useState([]);
+  const [index, setIndex] = useState(1);
 
-    const d = {
-      labels: [],
-      datasets: [
+  const d = {
+    labels: [],
+    datasets: [
+      {
+        label: "My First dataset",
+        fill: false,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        // pointRadius: 2,
+        pointHitRadius: 10,
+        data: chartData,
+        showLine: true,
+        borderWidth: 5,
+        lineTension: 0,
+        pointRadius: chartData.length
+          ? [...Array(chartData.length - 1).fill(2), 8]
+          : undefined,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      xAxes: [
         {
-          label: 'My First dataset',
-          fill: false,
-          backgroundColor: 'rgba(75,192,192,0.4)',
-          pointBorderColor: 'rgba(75,192,192,1)',
-          pointBackgroundColor: '#fff',
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          // pointRadius: 2,
-          pointHitRadius: 10,
-          data: chartData,
-          showLine: true,
-          borderWidth: 5,
-          lineTension: 0.2,
-          pointRadius: chartData.length ? [...Array(chartData.length-1).fill(2), 8]: undefined 
-        }
-      ],
-    
-    };
-    
-    const options = {
-      options: {
-        scales: {
-            xAxes: [{
-                type: 'linear',
-                position: 'center'
-            }],
-            yAxes: [{
-              type: 'linear',
-              position: 'center'
-          }]
+          ticks: {
+            stepSize: 1,
+          },
+          // position: 'center'
         },
-        animation: {
-          easing: 'linear'
+      ],
+      yAxes: [
+        {
+          ticks: {
+            stepSize: 1,
+          },
+          // position: 'center'
+        },
+      ],
+    },
+    animation: {
+      easing: "linear",
+    },
+    legend: {
+      display: false,
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      if (data[index] && index < data.length) {
+        console.log("HEY");
+        if (index > 1) {
+          setChartData([...chartData, data[index]]);
+        } else {
+          setChartData([["x", "y"], data[index]]);
         }
-    }
-    }
-
-    useEffect(() => {
-
-      const id = setTimeout(()=> {
-        if (data[index] && index < data.length){
-          console.log("HEY")
-          if (index > 1){
-              setChartData([...chartData, data[index]])
-          } else {
-              setChartData([['x', 'y'], data[index]])
-          }
-          setIndex(index + 1)
-          console.log([...chartData, data[index]])
+        setIndex(index + 1);
+        console.log([...chartData, data[index]]);
       }
-      }, 20)
-      
-      return () => {
-        clearTimeout(id)
-      }
-    }, [data, chartData, index])
-    
-    useEffect(() => {
-        console.log("rerender")
-        console.log(props.data)
-        setData(props.data)
-    }, [props.data])
-    // console.log(data)
+    }, 4);
 
-    return (
-        <>
-        {/* {JSON.stringify(chartData)} */}
-        <Scatter
-        data={d}
-        options={options}
+    return () => {
+      clearTimeout(id);
+    };
+  }, [data, chartData, index]);
 
-      />
-      </>
-    )
-}
+  useEffect(() => {
+    console.log("rerender");
+    console.log(props.data);
+    setData(props.data);
+  }, [props.data]);
+  // console.log(data)
+
+  return (
+    <>
+      {/* {JSON.stringify(chartData)} */}
+      <Scatter data={d} options={options} />
+    </>
+  );
+};
