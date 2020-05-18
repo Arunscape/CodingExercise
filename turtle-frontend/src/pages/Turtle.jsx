8c167fd3383ba4c926acc31bb8d51560d7b6df12
Arunscape <arun@woosaree.xyz>
2020-05-18 01:42:28 -0600

    todo, colour the duplicate points, make it look nice, and get a good night's sleep instead of doing random stuff all day then deciding midnight is the perfect time to start programming

--- turtle-frontend/src/pages/Turtle.jsx
+++ turtle-frontend/src/pages/Turtle.jsx
@@ -5,18 +5,22 @@ export default (props) => {
   const [chartDelay, setChartDelay] = useState(500);
 
   return (
-    <>
-      {chartDelay}
-      <input
-        type="range"
-        min="5"
-        max="5000"
-        onChange={(e) => {
-          setChartDelay(e.target.value);
-        }}
-      />
-      {"Speed (ms before drawing the next point, lower is faster)"}
-      <Chart data={props.data} chartDelay={chartDelay} />
-    </>
+    <div className="h-full w-full">
+      <div style={{ height: "20%" }}>
+        {chartDelay}
+        <input
+          type="range"
+          min="5"
+          max="5000"
+          onChange={(e) => {
+            setChartDelay(e.target.value);
+          }}
+        />
+        {"Speed (ms before drawing the next point, lower is faster)"}
+      </div>
+      <div style={{ height: "80%" }}>
+        <Chart className="w-full" data={props.data} chartDelay={chartDelay} />
+      </div>
+    </div>
   );
 };
